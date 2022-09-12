4c0025feeb2d0ef6402a3b64554fefb192ce95bd
Marcela Sarai Paz Bonilla (24204) <marcela.paz@est.zamorano.edu>
2022-09-12 17:25:39 -0600

    Segundo lab de TEA

---
+++ lab02/lab02- conversion de tipos.py
@@ -0,0 +1,8 @@
+# Tendencias e Innovacion en Tecnologia Agriocla (TEA)
+minutos = input("minutos jugados?")
+valorPorMinuto = input("valor por minuto?")
+
+# se utilza la conversion de tipo a int
+# sino se hacela conversion existira error potque trata de multripicar strings 
+# calculando el valor total de las minutos jugadas
+total = int(minutos) * int(valorPorMinuto)
